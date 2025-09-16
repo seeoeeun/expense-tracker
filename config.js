93d54d3949c79d61fa13b9be1fad6eeaa37c991f
seeoeeun <hamu920@gmail.com>
2025-09-16 11:13:40 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,17 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// config.js
+window.firebaseConfig = {
   apiKey: "AIzaSyDawZb7Wuj-9Rjd9sw6ofzrHIehdKEb9mc",
   authDomain: "expense-tracker-9e1c1.firebaseapp.com",
   projectId: "expense-tracker-9e1c1",
-  storageBucket: "expense-tracker-9e1c1.firebasestorage.app",
+  storageBucket: "expense-tracker-9e1c1.appspot.com",
   messagingSenderId: "467037427212",
   appId: "1:467037427212:web:c77b9f095325753f880029"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
